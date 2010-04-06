09bc0d0fe5defcd6a177c60e2902fd6579401cfb
Stephan Diehl <stephan@transvection.de>
2010-04-06 17:31:39 +0200

    new revision

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="AbletonVPath",
-    version="0.4.0",
+    version="0.4.1",
     description="A OO-abstraction of file-systems",
     author="Stephan Diehl",
     author_email="stephan.diehl@ableton.com",
