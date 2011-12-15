31069848e25427eacbbbdab88dcf8ba8ca400fff
Diez B. Roggisch <diez.roggisch@ableton.com>
2011-12-15 13:32:59 +0100

    replaced pkg_resources with stdlib's pkgutil

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="abl.vpath",
-    version="0.6.2",
+    version="0.6.3",
     description="A OO-abstraction of file-systems",
     author="Stephan Diehl",
     author_email="stephan.diehl@ableton.com",
