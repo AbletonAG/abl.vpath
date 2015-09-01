b0e9853b92a4103f017e1e36a09419909d0c4273
dir <dir@ableton.com>
2015-09-01 17:54:26 +0200

    Fix glob that wouldn't correctly deal with patterns like 'foo.*'

--- tests/test_misc.py
+++ tests/test_misc.py
@@ -5,11 +5,15 @@
 #******************************************************************************
 from __future__ import with_statement
 import os
+from unittest import TestCase
 
 from abl.vpath.base.misc import TempFileHandle
+from abl.vpath.base import URI
+
+from .common import CleanupMemoryBeforeTestMixin
 
 
-class TestTempFileHandle:
+class TestTempFileHandle(object):
     def test_handle(self):
         fs = open('testfile', 'w')
         fs.write('hallo')
@@ -19,3 +23,54 @@ class TestTempFileHandle:
         assert content == 'hallo'
         assert not os.path.exists('testfile')
 
+
+class TestGlob(CleanupMemoryBeforeTestMixin, TestCase):
+
+    def test_globbing_by_prefix(self):
+        base = URI("memory:///")
+        a = base / "a.foo"
+        b = base / "b.bar"
+
+        for f in [a, b]:
+            with f.open("w") as outf:
+                outf.write("foo")
+
+        self.assertEqual([a], base.glob("*.foo"))
+
+
+    def test_globbing_by_prefix_in_subdir(self):
+        base = URI("memory:///") / "dir"
+        base.mkdir()
+        a = base / "a.foo"
+        b = base / "b.bar"
+
+        for f in [a, b]:
+            with f.open("w") as outf:
+                outf.write("foo")
+
+        self.assertEqual([a], base.glob("*.foo"))
+
+
+    def test_globbing_by_suffix(self):
+        base = URI("memory:///")
+        a = base / "a.foo"
+        b = base / "b.bar"
+
+        for f in [a, b]:
+            with f.open("w") as outf:
+                outf.write("foo")
+
+        self.assertEqual([a], base.glob("a.*"))
+
+
+    def test_globbing_by_suffix_in_subdir(self):
+        base = URI("memory:///") / "dir"
+        base.mkdir()
+        a = base / "a.foo"
+        b = base / "b.bar"
+
+        for f in [a, b]:
+            with f.open("w") as outf:
+                outf.write("foo")
+
+        self.assertEqual([a], base.glob("a.*"))
